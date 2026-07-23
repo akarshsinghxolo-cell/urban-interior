b1a5ae958babab243c47aab89dbb2c6156bfe024
akarshsinghxolo-cell <akarshsinghxolo@gmail.com>
2026-07-23 16:57:09 +0530

    fix: allow system theme preference

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { UrbanCastleApp } from "@/components/urban-castle/UrbanCastleApp";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 export default function Home() {
-    return (<ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
+    return (<ThemeProvider attribute="class" defaultTheme="system" enableSystem={true} disableTransitionOnChange>
       <UrbanCastleApp />
       <Toaster richColors position="top-right"/>
     </ThemeProvider>);
