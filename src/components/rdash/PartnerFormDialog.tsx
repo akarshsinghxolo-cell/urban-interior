7a162530e96893fce29f98f18938ead6bc9121a5
akarshsinghxolo-cell <akarshsinghxolo@gmail.com>
2026-08-08 00:55:05 +0530

    Remove Vendor form compatibility bridge

--- src/components/rdash/PartnerFormDialog.tsx
+++ src/components/rdash/PartnerFormDialog.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import * as React from "react";
 import { ContractorFormDialog } from "./ContractorFormDialog";
 import { VendorFormDialog } from "./VendorFormDialog";
-import { retainPartnerFormStoreBridge } from "@/lib/rdash/partner-form-store-bridge";
 
 export type EntityType = "vendor" | "contractor";
 
@@ -14,11 +12,6 @@ export function EntityFormDialog(props: {
   onSaved?: (id: string) => void;
   editId?: string;
 }) {
-  React.useEffect(() => {
-    if (!props.open || props.type !== "vendor") return;
-    return retainPartnerFormStoreBridge("vendor", props.editId);
-  }, [props.open, props.type, props.editId]);
-
   if (props.type === "contractor") {
     return (
       <ContractorFormDialog
